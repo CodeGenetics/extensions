a48d81bdfa14ff5cd1331f89b433c66c5fd155f6
Junaid Ali <junaidali.exe@gmail.com>
2023-09-19 20:55:27 +0500

    Removed Paper

--- app/src/main/java/com/codegenetics/extensions/MainActivity.kt
+++ app/src/main/java/com/codegenetics/extensions/MainActivity.kt
@@ -4,7 +4,7 @@ import android.icu.util.LocaleData.PaperSize
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.codegenetics.extensions.paper.Paper
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
