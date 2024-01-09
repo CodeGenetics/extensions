9dd1fcaa2694f45eb94475ac3a23a92779facf58
Junaid Ali <junaidali.exe@gmail.com>
2024-01-09 11:02:49 +0500

    Rename BaseViewHolder

--- extensions/src/main/java/com/codegenetics/extensions/base/BaseViewHolder.kt
+++ extensions/src/main/java/com/codegenetics/extensions/base/BaseViewHolder.kt
@@ -1,6 +1,8 @@
 package com.codegenetics.extensions.base
 
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
-import androidx.viewbinding.ViewBinding
 
-open class BaseViewHolder<out T : ViewBinding>(val binding: T) : RecyclerView.ViewHolder(binding.root)
+abstract class BaseViewHolder<T>(binding: Any) : RecyclerView.ViewHolder(binding as View) {
+        abstract fun bind(data: T)
+    }
--- extensions/src/main/java/com/codegenetics/extensions/base/BaseViewMultiHolder.kt
+++
@@ -1,8 +0,0 @@
-package com.codegenetics.extensions.base
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-
-abstract class BaseViewMultiHolder<T>(binding: Any) : RecyclerView.ViewHolder(binding as View) {
-        abstract fun bind(data: T)
-    }
